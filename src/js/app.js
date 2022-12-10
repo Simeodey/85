b96505a5d79df1a68feaec9f05fb177f47a0ba6a
Simeodey <adamand12@abv.bg>
2022-12-10 14:55:51 +0200

    dasf

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,14 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+  .then(resp=>resp.json())
+  .then(data=>{
+    data.results.forEach(element => {
+      const li = document.createElement('li')
+      li.innerText = element.name
+      ul.appendChild(li)
+    });
+  })
+  .catch(console.error)
 });
